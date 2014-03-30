be9903b00e2e5f65c572f6e9dc37a14d8c702be8
Poul-Henning Kamp <phk@FreeBSD.org>
2014-03-30 09:46:03 +0000

    Found out what was wrong with TMS9900:  I had misunderstood an addressing mode.

--- examples/HP6034A/task.py
+++ examples/HP6034A/task.py
@@ -32,13 +32,90 @@ for i in fi:
 pj = pyreveng.job(m, "HP6034A")
 
 cx = cpu.tms9900.tms9981()
-cx.vectors(pj, xops = 6)
+cx.vectors(pj, xops = 0)
 
-for i in range(0, 12, 2):
-	data.dataptr(pj, 0x230 + i, 0x232 + i, pj.m.bu16(0x230 + i))
-	data.dataptr(pj, 0x248 + i, 0x24a + i, pj.m.bu16(0x248 + i))
-	data.dataptr(pj, 0x254 + i, 0x256 + i, pj.m.bu16(0x254 + i))
-	cx.codeptr(pj, 0x23c + i)
+def dptr(pj, a):
+	v = pj.m.bu16(a)
+	data.dataptr(pj, a, a + 2, v)
+	return v
+	
+
+i_args = {
+	0x79c:	"ii",
+	0x936:	"i",
+}
+
+def inline_args(ins):
+	for f in ins.flow_out:
+		i = i_args.get(f.to)
+		if i == None:
+			continue
+		ins.flow_out = []
+		ins.add_flow(pj, "C", True, f.to)
+		a = ins.hi
+		for j in i:
+			if j == "i":
+				d = pj.m.bu16(a)
+				data.dataptr(pj, a, a + 2, d)
+				a += 2
+			else:
+				print("IARG:", j)
+				assert False
+		ins.add_flow(pj, ">", True, a)
+		return
+
+cx.flow_check.append(inline_args)
+
+if True:
+
+	for i in range(0, 12, 2):
+		dptr(pj, 0x230 + i)
+		dptr(pj, 0x248 + i)
+		dptr(pj, 0x254 + i)
+		cx.codeptr(pj, 0x23c + i)
+
+if True:
+	cx.vector(pj, 0x364)
+	cx.vector(pj, 0x368)
+	pj.todo(0x037c, cx.disass)
+	pj.todo(0x038e, cx.disass)
+	pj.todo(0x03aa, cx.disass)
+	pj.todo(0x03ba, cx.disass)
+	pj.todo(0x03ca, cx.disass)
+	pj.todo(0x0406, cx.disass)
+	pj.todo(0x0428, cx.disass)
+	pj.todo(0x044a, cx.disass)
+	pj.todo(0x0458, cx.disass)
+	pj.todo(0x052e, cx.disass)
+
+if True:
+	# Pure guesswork
+	for a in range(0xcee, 0xd0c, 2):
+		cx.codeptr(pj, a)
+	pj.todo(0x0e1e, cx.disass)
+
+if True:
+	pj.todo(0x0d18, cx.disass)
+	
+if True:
+	pj.todo(0x0e50, cx.disass)
+	pj.todo(0x0e80, cx.disass)
+
+if True:
+	pj.todo(0x10b2, cx.disass)
+
+if True:
+	for a in range(0xf4e, 0xf54, 2):
+		cx.codeptr(pj, a)
+
+if True:
+	for a in range(0xf3e, 0xf42, 2):
+		cx.codeptr(pj, a)
+
+if True:
+	for a in range(0x468, 0x474, 2):
+		v = dptr(pj, a)
+		cx.vector(pj, v)
 
 while pj.run():
         pass
---
+++ pyreveng/cpu/tms9900.py
@@ -0,0 +1,240 @@
+#!/usr/bin/env python
+#
+# Disassembler for TI TMS9900 microprocessor
+#
+
+
+from __future__ import print_function
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join("..")))
+
+import pyreveng
+import instree
+import assy
+
+tms9900_instructions = """
+A	b,so,do	|1 0 1|b|td | d     |ts | s     |
+C	b,so,do	|1 0 0|b|td | d     |ts | s     |
+S	b,so,do	|0 1 1|b|td | d     |ts | s     |
+SOC	b,so,do	|1 1 1|b|td | d     |ts | s     |
+SZC	b,so,do	|0 1 0|b|td | d     |ts | s     |
+MOV	b,so,do	|1 1 0|b|td | d     |ts | s     |
+
+COC	so,w	|0 0 1 0 0 0| w     |ts | s     |
+CZC	so,w	|0 0 1 0 0 1| w     |ts | s     |
+XOR	so,w	|0 0 1 0 1 0| w     |ts | s     |
+MPY	so,w	|0 0 1 1 1 0| w     |ts | s     |
+DIV	so,w	|0 0 1 1 1 1| w     |ts | s     |
+
+XOP	?	|0 0 1 0 1 1| d     |ts | s     |
+
+B	da,>J	|0 0 0 0 0 1 0 0 0 1|1 0|0 0 0 0| da				|
+B	so,>J	|0 0 0 0 0 1 0 0 0 1|ts | s     |
+BL	da,>C	|0 0 0 0 0 1 1 0 1 0|1 0|0 0 0 0| da				|
+BL	so,>C	|0 0 0 0 0 1 1 0 1 0|ts | s     |
+BLWP	ptr,>C	|0 0 0 0 0 1 0 0 0 0|1 0|0 0 0 0| ptr				|
+BLWP	so	|0 0 0 0 0 1 0 0 0 0|ts | s     |
+
+CLR	so	|0 0 0 0 0 1 0 0 1 1|ts | s     |
+SETO	so	|0 0 0 0 0 1 1 1 0 0|ts | s     |
+INV	so	|0 0 0 0 0 1 0 1 0 1|ts | s     |
+NEG	so	|0 0 0 0 0 1 0 1 0 0|ts | s     |
+ABS	so	|0 0 0 0 0 1 1 1 0 1|ts | s     |
+SWPB	so	|0 0 0 0 0 1 1 0 1 1|ts | s     |
+
+INC	so	|0 0 0 0 0 1 0 1 1 0|ts | s     |
+INCT	so	|0 0 0 0 0 1 0 1 1 1|ts | s     |
+DEC	so	|0 0 0 0 0 1 1 0 0 0|ts | s	|
+DECT	so	|0 0 0 0 0 1 1 0 0 1|ts | s	|
+X	so	|0 0 0 0 0 1 0 0 1 0|ts | s	|
+
+LDCR	c,so	|0 0 1 1 0 0| c     |ts | s     |
+STCR	c,so	|0 0 1 1 0 1| c     |ts | s     |
+
+SBO	disp	|0 0 0 1 1 1 0 1| disp		|
+SBZ	disp	|0 0 0 1 1 1 1 0| disp		|
+TB	disp	|0 0 0 1 1 1 1 1| disp		|
+
+JEQ	r,>JC	|0 0 0 1 0 0 1 1| disp		|
+JGT	r,>JC	|0 0 0 1 0 1 0 1| disp		|
+JH	r,>JC	|0 0 0 1 1 0 1 1| disp		|
+JHE	r,>JC	|0 0 0 1 0 1 0 0| disp		|
+JL	r,>JC	|0 0 0 1 1 0 1 0| disp		|
+JLE	r,>JC	|0 0 0 1 0 0 1 0| disp		|
+JLT	r,>JC	|0 0 0 1 0 0 0 1| disp		|
+JMP	r,>J	|0 0 0 1 0 0 0 0| disp		|
+JNC	r,>JC	|0 0 0 1 0 1 1 1| disp		|
+JNE	r,>JC	|0 0 0 1 0 1 1 0| disp		|
+JNO	r,>JC	|0 0 0 1 1 0 0 1| disp		|
+JOC	r,>JC	|0 0 0 1 1 0 0 0| disp		|
+JOP	r,>JC	|0 0 0 1 1 1 0 0| disp		|
+
+SLA	sc,w	|0 0 0 0 1 0 1 0| c     | w	|
+SRA	sc,w	|0 0 0 0 1 0 0 0| c     | w	|
+SRC	sc,w	|0 0 0 0 1 0 1 1| c     | w	|
+SRL	sc,w	|0 0 0 0 1 0 0 1| c     | w	|
+
+AI	w,i	|0 0 0 0 0 0 1 0 0 0 1|n| w	| iop				|
+ANDI	w,i	|0 0 0 0 0 0 1 0 0 1 0|n| w	| iop				|
+CI	w,i	|0 0 0 0 0 0 1 0 1 0 0|n| w	| iop				|
+LI	w,i	|0 0 0 0 0 0 1 0 0 0 0|n| w	| iop				|
+ORI	w,i	|0 0 0 0 0 0 1 0 0 1 1|n| w	| iop				|
+
+LWPI	i	|0 0 0 0 0 0 1 0 1 1 1|0 0 0 0 0| iop				|
+LIMI	i	|0 0 0 0 0 0 1 1 0 0 0|0 0 0 0 0| iop				|
+
+STST	w	|0 0 0 0 0 0 1 0 1 1 0|0| w	|
+STWP	?	|0 0 0 0 0 0 1 0 1 0 1|n| w	|
+
+RTWP	>R	|0 0 0 0 0 0 1 1 1 0 0| n	|
+
+IDLE	?	|0 0 0 0 0 0 1 1 0 1 0| n	|
+RSET	?	|0 0 0 0 0 0 1 1 0 1 1| n	|
+CKOF	?	|0 0 0 0 0 0 1 1 1 1 0| n	|
+CKON	?	|0 0 0 0 0 0 1 1 1 0 1| n	|
+LREX	?	|0 0 0 0 0 0 1 1 1 1 1| n	|
+
+"""
+
+import data
+
+
+def arg_o(pj, ins, to, o):
+	if to == 0:
+		return assy.arg_verbatim(pj, "R%d" % o)
+	if to == 1:
+		return assy.arg_verbatim(pj, "*R%d" % o)
+
+	if to == 2:
+		v = pj.m.bu16(ins.hi)
+		ins.hi += 2
+		if o == 0:
+			return assy.arg_verbatim(pj, "@0x%04x" % v)
+		else:
+			return assy.arg_verbatim(pj, "R%d+#0x%04x" % (o, v))
+
+	if to == 3:
+		return assy.arg_verbatim(pj, "*R%d+" % o)
+	
+def arg_so(pj, ins):
+	return arg_o(pj, ins, ins.im.F_ts, ins.im.F_s)
+
+def arg_do(pj, ins):
+	return arg_o(pj, ins, ins.im.F_td, ins.im.F_d)
+
+def arg_b(pj, ins):
+	if ins.im.F_b:
+		ins.mne += "B"
+
+class arg_da(assy.arg_dst):
+        def __init__(self, pj, ins):
+		ins.dstadr = ins.im.F_da
+		super(arg_da, self).__init__(pj, ins.dstadr)
+
+class arg_r(assy.arg_dst):
+        def __init__(self, pj, ins):
+		i = ins.im.F_disp
+		if i & 0x80:
+			i -= 256
+		ins.dstadr = ins.hi + i * 2
+		super(arg_r, self).__init__(pj, ins.dstadr)
+
+class arg_c(object):
+	def __init__(self, pj, ins):
+		self.val = ins.im.F_c
+
+	def render(self, pj):
+		return "#0x%x" % self.val
+
+class arg_ptr(assy.arg_dst):
+	def __init__(self, pj, ins):
+		self.val = ins.im.F_ptr
+		c = ins.lang.vector(pj, self.val)
+		ins.dstadr = c.c.dst
+		super(arg_ptr, self).__init__(pj, ins.dstadr)
+
+	def render(self, pj):
+		return "@0x%04x" % self.val
+
+class arg_i(object):
+	def __init__(self, pj, ins):
+		self.val = ins.im.F_iop
+
+	def render(self, pj):
+		return "@0x%04x" % self.val
+
+class arg_w(assy.arg_verbatim):
+	def __init__(self, pj, ins):
+		super(arg_w, self).__init__(pj, "R%d" % ins.im.F_w)
+
+class arg_disp(assy.arg_verbatim):
+	def __init__(self, pj, ins):
+		i = ins.im.F_disp
+		if i & 0x80:
+			i -= 0x100
+		super(arg_disp, self).__init__(pj, "%+d" % i)
+
+def arg_sc(pj, ins):
+	if ins.im.F_c == 0:
+		return assy.arg_verbatim(pj, "R0")
+	else:
+		return assy.arg_verbatim(pj, "#%d" % ins.im.F_c)
+
+class tms9900(assy.instree_disass):
+	def __init__(self):
+		super(tms9900, self).__init__("TMS 9900", 16, 8, ">")
+		self.it.load_string(tms9900_instructions)
+		self.n_interrupt = 16
+		self.args.update( {
+		    "r":	arg_r,
+		    "i":	arg_i,
+		    "ptr":	arg_ptr,
+		    "w":	arg_w,
+		    "b":	arg_b,
+		    "c":	arg_c,
+		    "so":	arg_so,
+		    "do":	arg_do,
+		    "da":	arg_da,
+		    "sc":	arg_sc,
+		    "disp":	arg_disp,
+		} )
+
+	def vector(self, pj, adr):
+		c = pj.find(adr, "vector")
+		if c == None:
+			c = pj.add(adr, adr + 4, "vector")
+			c.d = data.dataptr(pj, adr + 0x00, adr + 0x02, pj.m.bu16(adr))
+			c.c = self.codeptr(pj, adr + 0x02)
+		return c
+
+	def codeptr(self, pj, adr):
+		t = pj.m.bu16(adr)
+		c = data.codeptr(pj, adr, adr + 2, t)
+		pj.todo(t, self.disass)
+		return c
+
+	def vectors(self, pj, adr = 0x0, xops = 1):
+		def vect(pj, a, lbl):
+			c = self.vector(pj, a)
+			pj.set_label(c.c.dst, lbl)
+			return c
+		
+		c = vect(pj, adr, "RESET")
+		for i in range(1, self.n_interrupt):
+			if pj.m.bu16(i * 4) != 0:
+				c = vect(pj, i * 4, "INT%d" % i)
+		for i in range(xops):
+			c = vect(pj, 0x40 + i * 4, "XOP%d" % i)
+
+class tms9981(tms9900):
+	def __init__(self):
+		super(tms9981, self).__init__()
+		self.n_interrupt = 5
+		
+
+if __name__ == "__main__":
+	h = tms9900()
+	h.it.print()
