99d470d81f096b0c4785ebfb53921612b59e6456
Poul-Henning Kamp <phk@FreeBSD.org>
2016-07-03 21:00:03 +0000

    Take menus properly apart

--- examples/HP8904A/example.py
+++ examples/HP8904A/example.py
@@ -158,43 +158,82 @@ hpib = {
 
 def symb(pj, cpu):
 	for p,a,n in [
+		(0, 0x4018, "MAIN_MENUS"),
+		(0, 0x43bd, "HOP_RAM_ADRS"),
+		(0, 0x4908, "HOP_CFG"),
 		(0, 0x4ed8, "0x4ed8"),		# @0:54cb -> 3b85
 		(0, 0x51e3, "0x51e3"),		# @1:55bc -> 2213
 		(0, 0x5253, "0x5253"),		# @0:54d1 -> 2213
 		(0, 0x5d0c, "0x5d0c"),		# @0:54bf -> 220f
+
 		(1, 0x42d1, "0x42d1"),		# @1:6cc4
 		(1, 0x444c, "0x444c"),		# @1:6cb1
 		(1, 0x4567, "0x4567"),		# @1:5517
 		(1, 0x4c01, "0x4c01"),		# @1:550b -> 220f
 		(1, 0x5185, "0x5185"),		# @1:5511 -> 2211
 		(1, 0x51e3, "0x51e3"),		# @1:55bf -> 2213
-		(1, 0x55c5, "IRQ_XX1"),
+		(1, 0x55c5, "APP_TONE"),
 		(1, 0x7870, "0x7870"),		# @1:79e5
-		(1, 0x7a81, "IRQ_XX2"),
+		(1, 0x7a81, "APP_DTMF"),
+		(1, 0x47fa, "TONE_NBR"),
+		(1, 0x4842, "TONE_ON_TIME"),
+		(1, 0x492c, "TONE_F3"),
+		(1, 0x4860, "TONE_OFF_TIME"),
+		(1, 0x71b4, "TSEQ_INDEX"),
+		(1, 0x726a, "TSEQ_HEX"),
+		(1, 0x7203, "TSEQ_EDIT"),
+		(1, 0x71de, "TSEQ_END"),
+		(1, 0x49ee, "TSEQ_NBR"),
+		(1, 0x4a19, "TSEQ_CONT"),
+		(1, 0x4aa2, "TSEQ_SINGLE"),
+		(1, 0x4ab9, "TSEQ_STOP"),
+
 		(2, 0x4196, "0x4196"),		# @2:543a
 		(2, 0x642e, "0x642e"),		# @2:6581
 		(2, 0x6646, "0x6646"),		# @2:72f0
 		(2, 0x6ad0, "0x6ad0"),		# @2:72e4 -> 220f
 		(2, 0x6f60, "0x6f60"),		# @2:72ea -> 2211
 		(2, 0x6fbe, "0x6fbe"),		# @2:735b -> 2213
-		(2, 0x7364, "IRQ_XX3"),
+		(2, 0x7364, "APP_DSEQ"),
+		(2, 0x689b, "DSEQ_BASE"),
+		(2, 0x6940, "DSEQ_ON_LEVEL"),
+		(2, 0x6917, "DSEQ_PERIOD"),
+		(2, 0x6976, "DSEQ_OFF_LEVEL"),
+		(2, 0x5b24, "DSEQ_INDEX"),
+		(2, 0x5b4e, "DSEQ_END"),
+		(2, 0x5bda, "DSEQ_HEX"),
+		(2, 0x5b73, "DSEQ_EDIT"),
+		(2, 0x69ed, "DSEQ_BIT"),
+		(2, 0x6a18, "DSEQ_CONT"),
+		(2, 0x6aa1, "DSEQ_SINGLE"),
+		(2, 0x6ab8, "DSEQ_STOP"),
+
+		(3, 0x471c, "ROM_CHECK"),
 		(3, 0x4803, "0x4803"),		# @3:4b72 -> 23b0
 		(3, 0x4848, "0x4848"),
+		(3, 0x4b1e, "KEYBOARD_CHECK"),
 		(3, 0x4b85, "0x4b85"),		# @3:4c51 -> 220f
-		(3, 0x4c26, "IRQ_XX4"),
+		(3, 0x4c26, "APP_DIAG"),
+		(3, 0x4dcf, "RAM_CHECK"),
+		(3, 0x4ef7, "LCD_CHECK"),
 		(3, 0x50ce, "0x50ce"),		# @3:52f2 -> 220f
+		(3, 0x5298, "MEM_ACCESS"),
+		(3, 0x530f, "PLL_CHECK"),
 		(3, 0x5914, "0x5914"),		# @3:5f7f -> 220f
+		(3, 0x5ec8, "MANUAL_COWCHIP"),
 		(3, 0x5f96, "0x5f96"),		# @3:6230
+		(3, 0x61bb, "MANUAL_MEMORY"),
 		(3, 0x6a25, "0x6a25"),		# @3:7323 -> 220d
 		(3, 0x6d64, "0x6d64"),		# @3:6f6d -> 220d
 		(3, 0x6f80, "0x6f80"),		# @3:7233 -> 220d
-		(3, 0x7246, "IRQ_XX5"),
-		(3, 0x7ac3, "IRQ_XX6"),
+		(3, 0x7246, "APP_XX5"),
+		(3, 0x7ac3, "APP_XX6"),
 		(3, 0x7b7a, "0x7b7a"),
+
 		(4, 0x89aa, "0x89aa"),
-		(4, 0x8a08, "0x8a08"),
+		(4, 0x8a08, "SET_MENU"),
 		(4, 0x8a2f, "0x8a2f"),
-		(4, 0x8b4c, "0x8b4c"),
+		(4, 0x8b4c, "MENU_EXIT"),
 		(4, 0x8c14, "0x8c14"),		# @0x8d51,0x8edb -> 220f
 		(4, 0x90c9, "0x90c9"),
 		(4, 0x9a40, "0x9a40"),		# #0x9199
@@ -215,6 +254,8 @@ def symb(pj, cpu):
 		(4, 0xb8cc, "0xb8cc"),
 		(4, 0xc1e3, "0xc1e3"),
 		(4, 0xc239, "NULL"),
+		(4, 0xc242, "MEMCMP"),
+		(4, 0xc285, "MEMCPY"),
 		(4, 0xc418, "0xc418"),
 		(4, 0xc7b0, "0xc7b0"),
 		(4, 0xc855, "0xc855"),
@@ -238,13 +279,14 @@ def symb(pj, cpu):
 		(4, 0xd781, "LCD_RD_CTRL"),
 		(4, 0xd7a0, "LCD_DDRAM"),
 		(4, 0xd8ea, "DISPLAY"),
+		(4, 0xd989, "DISPLAY2L"),
 		(4, 0xd9e6, "LCD_INIT"),
 		(4, 0xdae0, "BANKSWITCH"),
-		(4, 0xdb2b, "0xdb2b"),
+		(4, 0xdb2b, "CALL_BANK"),
 		(4, 0xdc7b, "RAM_ROM_TEST"),
 		(4, 0xdc82, "RAM_TEST"),
 		(4, 0xdca9, "ROM_SUM"),
-		(4, 0xdd1e, "0xdd1e"),
+		(4, 0xdd1e, "APP_JMP"),
 		(4, 0xdeb6, "0xdeb6"),
 		(4, 0xe029, "0xe029"),
 		(4, 0xe185, "0xe185"),
@@ -258,10 +300,16 @@ def symb(pj, cpu):
 		(4, 0xf7ec, "0xf7ec"),
 		(4, 0xf9d4, "0xf9d4"),
 		(4, 0xfd50, "PROLOGUE"),
+
 		(6, 0x0300, "LCD_CTL"),
 		(6, 0x0301, "LCD_DATA"),
-		(6, 0x247c, "IRQ_BANK"),
-		(6, 0x247d, "IRQ_VECTOR"),
+		(6, 0x2223, "MAIN_MENU_LAST_PG"),
+		(6, 0x2240, "MENU_PTR"),
+		(6, 0x2245, "MAIN_MENU_CUR_PG"),
+		(6, 0x2242, "MENU_CNT"),
+		(6, 0x247b, "CUR_BANK"),
+		(6, 0x247c, "APP_BANK"),
+		(6, 0x247d, "APP_VECTOR"),
 		(6, 0x3ffc, "OPTIONS"),
 		(6, 0xed11, "blank_line"),
 	]:
@@ -391,20 +439,58 @@ def do_switch():
 
 #######################################################################
 
+class Num(data.Data):
+	def __init__(self, pj, lo):
+		super(Num, self).__init__(pj, lo, lo + 3, "Num")
+		a = pj.m.rd(lo) << 16
+		a += pj.m.rd(lo + 1) << 8
+		a += pj.m.rd(lo + 2)
+		self.fmt = ".NUM\t%d" % a
+		pj.set_label(lo, "N%d" % a)
+
+#######################################################################
+
+class MenuPage(data.Data):
+	def __init__(self, pj, cpu, lo):
+		super(MenuPage, self).__init__(pj, lo, lo + 10, "MenuPage")
+		t = pj.m.bu16(lo)
+		y1 = data.Txt(pj, t, t + 40, label=False)
+		y1.compact = True
+		y2 = data.Txt(pj, t + 40, t + 80, label=False)
+		y2.compact = True
+		self.t1 = y1.txt
+		self.t2 = y2.txt
+		self.ptr = []
+		for i in range(1, 5):
+			u = pj.m.bu16(lo + 2 * i)
+			self.ptr.append(u)
+			if u >= pj.m.lo and u < pj.m.hi:
+				cpu.disass(pj, u)
+
+	def render(self, pj):
+		s = "MENUPAGE {\n"
+		s += "\t.txt1 = \"%s\"\n" % self.t1
+		s += "\t.txt2 = \"%s\"\n" % self.t2
+		for i in range(4):
+			s += "\t.f%d = " % (i + 1)
+			s += "%s\n" % pj.render_adr(self.ptr[i])
+		s += "}"
+		return s
+
+def Menu(pj, cpu, a, nm):
+	pj.set_label(a, nm)
+	data.Const(pj, a - 1, a)
+	n = pj.m.rd(a - 1)
+	for i in range(0, n + 1):
+		MenuPage(pj, cpu, a + i * 10)
+
+#######################################################################
+
 def hints(pj, cpu):
 
-	def tbl0_0(pj, cpu, a):
-		data.Dataptr(pj, a, a + 2, pj.m.bu16(a))
-		u = pj.m.bu16(a)
-		y = data.Txt(pj, u, u + 40, label=False)
-		y.compact = True
-		y = data.Txt(pj, u + 40, u + 80, label=False)
-		y.compact = True
-		for b in range(2, 10, 2):
-			cpu.codeptr(pj, a + b)
-
-
 	if pj.pg == 0:
+		Menu(pj, cpu, 0x433e, "CHANMENU")
+
 		# @0x8948
 		data.Const(pj, 0x4003, 0x4004)
 
@@ -437,15 +523,11 @@ def hints(pj, cpu):
 		y = data.Txt(pj, 0x41bb, 0x41bb + 12, label=False)
 		y.compact = True
 
-		# @0x5454
-		for a in range(0x433e, 0x4384, 10):
-			tbl0_0(pj, cpu, a)
 
 	if pj.pg == 1:
-		for a in range(0x416b, 0x4193, 10):
-			tbl0_0(pj, cpu, a)
-		for a in range(0x4234, 0x425c, 10):
-			tbl0_0(pj, cpu, a)
+		Menu(pj, cpu, 0x416b, "TONEMENU")
+		Menu(pj, cpu, 0x4234, "DTMFMENU")
+
 		for a in (0x69a6,):
 			y = data.Txt(pj, a, a + 0x28, label=False)
 			y.compact = True
@@ -456,16 +538,15 @@ def hints(pj, cpu):
 			data.Const(pj, a, a + 3)
 
 	if pj.pg == 2:
-		for a in range(0x416b, 0x4193, 10):
-			tbl0_0(pj, cpu, a)
+		Menu(pj, cpu, 0x416b, "DSEQMENU")
 		for a in range(0x4245, 0x4255, 2):
 			cpu.codeptr(pj, a)
 		for a in range(0x4330, 0x4340, 2):
 			cpu.codeptr(pj, a)
 
 	if pj.pg == 3:
-		for a in range(0x4178, 0x41a0, 10):
-			tbl0_0(pj, cpu, a)
+		Menu(pj, cpu, 0x4178, "DIAGMENU")
+		MenuPage(pj, cpu, 0x4196)
 		for i in range(0x3f):
 			a = 0x7349 + 4 * i
 			y = data.Const(pj, a, a + 2)
@@ -555,10 +636,20 @@ def hints(pj, cpu):
 			y = data.Const(pj, a, a + 8, fmt="0x%02x")
 
 		for a,b in (
-			(0x8f7c,36),
+			(0x8f7c,35),
 			(0x977b, 5),
 			(0xed11,40),
 			(0xea99,0x23),
+			(0xeae4,13),
+			(0xeb41,40),
+			(0xeb69,40),
+			(0xec5b,14),
+			(0xec69,14),
+			(0xec77,14),
+			(0xec85,0x36),
+			(0xecbb,6),
+			(0xecc1,40),
+			(0xece9,40),
 		):
 			y = data.Txt(pj, a, a + b, label=False)
 			y.compact = True
@@ -576,7 +667,7 @@ def hints(pj, cpu):
 					x = x << 1
 
 		l = [ "LCD_CHR_f", "LCD_CHR_1", "LCD_CHR_2", "LCD_CHR_3",
-		      "LCD_CHR_4", "LCD_CHR_oe", "LCD_CHR_mu", "LCD_CHR_is", ]
+		      "LCD_CHR_4", "LCD_CHR_phi", "LCD_CHR_mu", "LCD_CHR_is", ]
 		for a in range(0xea15, 0xea4e, 8):
 			pj.set_label(a, l.pop(0))
 			char_def(pj, a)
@@ -585,7 +676,16 @@ def hints(pj, cpu):
 		data.Const(pj, 0x9777, 0x9777 + 4)
 		data.Const(pj, 0xdd73, 0xdd9d)
 
+		# 2 x 35 bytes, no idea...
+		for a in range(0xed89, 0xedcf, 5):
+			data.Const(pj, a, a + 5, "0x%02x")
 
+		for a in range(0xee47, 0xee62, 3):
+			Num(pj, a)
+
+		for a in (0xea55, 0xea58, 0xea5f, 0xea62, 0xea65,
+		    0xea68, 0xea6b, 0xea6e):
+			Num(pj, a)
 
 
 #######################################################################
