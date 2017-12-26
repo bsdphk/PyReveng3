cc22440ee33e5e15fff8cd31db99e0896a1728a9
Poul-Henning Kamp <phk@FreeBSD.org>
2017-12-26 16:23:07 +0000

    Resort and collapse A/B

--- pyreveng/cpu/mc6809.py
+++ pyreveng/cpu/mc6809.py
@@ -48,6 +48,11 @@ NEG	d	|0 0 0 0 0 0 0 0| d		| {
 	FLG UXXXX %2 sub %1 1
 }
 
+NEG	P	|0 1 1 0 0 0 0 0|X| R |i| m	|
+
+NEG	E	|0 1 1 1 0 0 0 0| e1		| e2		|
+
+
 COM	d	|0 0 0 0 0 0 1 1| d		| {
 	%1 = load i8 , i8* D
 	%2 = xor i8 %1 , 0xff
@@ -55,15 +60,39 @@ COM	d	|0 0 0 0 0 0 1 1| d		| {
 	FLG UXX01 %2
 }
 
+COM	P	|0 1 1 0 0 0 1 1|X| R |i| m	|
+
+COM	E	|0 1 1 1 0 0 1 1| e1		| e2		|
+
 LSR	d	|0 0 0 0 0 1 0 0| d		|
+
+LSR	P	|0 1 1 0 0 1 0 0|X| R |i| m	|
+
+LSR	E	|0 1 1 1 0 1 0 0| e1		| e2		|
 
 ROR	d	|0 0 0 0 0 1 1 0| d		|
 
+ROR	P	|0 1 1 0 0 1 1 0|X| R |i| m	|
+
+ROR	E	|0 1 1 1 0 1 1 0| e1		| e2		|
+
 ASR	d	|0 0 0 0 0 1 1 1| d		|
+
+ASR	P	|0 1 1 0 0 1 1 1|X| R |i| m	|
+
+ASR	E	|0 1 1 1 0 1 1 1| e1		| e2		|
 
 ASL	d	|0 0 0 0 1 0 0 0| d		|
 
+ASL	P	|0 1 1 0 1 0 0 0|X| R |i| m	|
+
+ASL	E	|0 1 1 1 1 0 0 0| e1		| e2		|
+
 ROL	d	|0 0 0 0 1 0 0 1| d		|
+
+ROL	P	|0 1 1 0 1 0 0 1|X| R |i| m	|
+
+ROL	E	|0 1 1 1 1 0 0 1| e1		| e2		|
 
 DEC	d	|0 0 0 0 1 0 1 0| d		| {
 	%1 = load i8 , i8* D
@@ -72,12 +101,22 @@ DEC	d	|0 0 0 0 1 0 1 0| d		| {
 	FLG -XXX- %2 sub %1 1
 }
 
+DEC	P	|0 1 1 0 1 0 1 0|X| R |i| m	|
+
+DEC	E	|0 1 1 1 1 0 1 0| e1		| e2		|
+
+
 INC	d	|0 0 0 0 1 1 0 0| d		| {
 	%1 = load i8 , i8* D
 	%2 = add i8 %1 , 1
 	store i8 %2 , i8* D
 	FLG -XXX- %2 add %1 1
 }
+
+INC	P	|0 1 1 0 1 1 0 0|X| R |i| m	|
+
+INC	E	|0 1 1 1 1 1 0 0| e1		| e2		|
+
 
 TST	d	|0 0 0 0 1 1 0 1| d		| {
 	%1 = load i8 , i8* D
@@ -87,6 +126,10 @@ TST	d	|0 0 0 0 1 1 0 1| d		| {
 JMP	d,>J	|0 0 0 0 1 1 1 0| d		| {
 	br label D
 }
+
+JMP	P,>J	|0 1 1 0 1 1 1 0|X| R |i| m	|
+
+JMP	E,>J	|0 1 1 1 1 1 1 0| e1		| e2		|
 
 CLR	d	|0 0 0 0 1 1 1 1| d		| {
 	store i8 0 , i8* D
@@ -104,6 +147,7 @@ CMPD	I	|0 0 0 1 0 0 0 0|1 0 0 0 0 0 1 1| I1		| I2		|
 CMPD	d	|0 0 0 1 0 0 0 0|1 0 0 1 0 0 1 1| d		|
 
 CMPD    P	|0 0 0 1 0 0 0 0|1 0 1 0 0 0 1 1|X| R |i| m     |
+
 CMPD	E	|0 0 0 1 0 0 0 0|1 0 1 1 0 0 1 1| e1		| e2		|
 
 SWI3	>J	|0 0 0 1 0 0 0 1|0 0 1 1 1 1 1 1|
@@ -198,132 +242,71 @@ MUL	-	|0 0 1 1 1 1 0 1|
 
 SWI	-	|0 0 1 1 1 1 1 1|
 
-NEGA	-	|0 1 0 0 0 0 0 0|
+NEG	AB	|0 1 0|a|0 0 0 0|
 
-COMA	-	|0 1 0 0 0 0 1 1|
+COM	AB	|0 1 0|a|0 0 1 1|
 
-LSRA	-	|0 1 0 0 0 1 0 0|
+LSR	AB	|0 1 0|a|0 1 0 0|
 
-RORA	-	|0 1 0 0 0 1 1 0|
+ROR	AB	|0 1 0|a|0 1 1 0|
 
-ASRA	-	|0 1 0 0 0 1 1 1|
+ASR	AB	|0 1 0|a|0 1 1 1|
 
-ASLA	-	|0 1 0 0 1 0 0 0|
+ASL	AB	|0 1 0|a|1 0 0 0|
 
-ROLA	-	|0 1 0 0 1 0 0 1|
+ROL	AB	|0 1 0|a|1 0 0 1|
 
-DECA	-	|0 1 0 0 1 0 1 0|
+DEC	AB	|0 1 0|a|1 0 1 0|
 
-INCA	-	|0 1 0 0 1 1 0 0|
+INC	AB	|0 1 0|a|1 1 0 0|
 
-TSTA	-	|0 1 0 0 1 1 0 1|
+TST	AB	|0 1 0|a|1 1 0 1|
 
-CLRA	-	|0 1 0 0 1 1 1 1| {
-	%A = i8 0
+CLR	AB	|0 1 0|a|1 1 1 1| {
+	AB = i8 0
 	FLG -0100
 }
-
-NEGB	-	|0 1 0 1 0 0 0 0|
-
-COMB	-	|0 1 0 1 0 0 1 1|
-
-LSRB	-	|0 1 0 1 0 1 0 0|
-
-RORB	-	|0 1 0 1 0 1 1 0|
-
-ASRB	-	|0 1 0 1 0 1 1 1|
-
-ASLB	-	|0 1 0 1 1 0 0 0|
-
-ROLB	-	|0 1 0 1 1 0 0 1|
-
-DECB	-	|0 1 0 1 1 0 1 0|
-
-INCB	-	|0 1 0 1 1 1 0 0|
-
-TSTB	-	|0 1 0 1 1 1 0 1|
-
-CLRB	-	|0 1 0 1 1 1 1 1| {
-	%B = i8 0
-	FLG -0100
-}
-
-NEG	P	|0 1 1 0 0 0 0 0|X| R |i| m	|
-
-COM	P	|0 1 1 0 0 0 1 1|X| R |i| m	|
-
-LSR	P	|0 1 1 0 0 1 0 0|X| R |i| m	|
-
-ROR	P	|0 1 1 0 0 1 1 0|X| R |i| m	|
-
-ASR	P	|0 1 1 0 0 1 1 1|X| R |i| m	|
-
-ASL	P	|0 1 1 0 1 0 0 0|X| R |i| m	|
-
-ROL	P	|0 1 1 0 1 0 0 1|X| R |i| m	|
-
-DEC	P	|0 1 1 0 1 0 1 0|X| R |i| m	|
-
-INC	P	|0 1 1 0 1 1 0 0|X| R |i| m	|
 
 TST	P	|0 1 1 0 1 1 0 1|X| R |i| m	|
 
-JMP	P,>J	|0 1 1 0 1 1 1 0|X| R |i| m	|
+TST	E	|0 1 1 1 1 1 0 1| e1		| e2		|
 
 CLR	P	|0 1 1 0 1 1 1 1|X| R |i| m	|
 
-NEG	E	|0 1 1 1 0 0 0 0| e1		| e2		|
-
-COM	E	|0 1 1 1 0 0 1 1| e1		| e2		|
-
-LSR	E	|0 1 1 1 0 1 0 0| e1		| e2		|
-
-ROR	E	|0 1 1 1 0 1 1 0| e1		| e2		|
-
-ASR	E	|0 1 1 1 0 1 1 1| e1		| e2		|
-
-ASL	E	|0 1 1 1 1 0 0 0| e1		| e2		|
-
-ROL	E	|0 1 1 1 1 0 0 1| e1		| e2		|
-
-DEC	E	|0 1 1 1 1 0 1 0| e1		| e2		|
-
-INC	E	|0 1 1 1 1 1 0 0| e1		| e2		|
-
-TST	E	|0 1 1 1 1 1 0 1| e1		| e2		|
-
-JMP	E,>J	|0 1 1 1 1 1 1 0| e1		| e2		|
-
 CLR	E	|0 1 1 1 1 1 1 1| e1		| e2		|
 
-SUBA	i	|1 0 0 0 0 0 0 0| i		| {
-	%1 = i8 %A
-	%A = sub i8 %A , I
-	FLG UXXXX %A sub %1 I
+
+SUB	AB,i	|1|a|0 0 0 0 0 0| i		| {
+	%1 = i8 AB
+	AB = sub i8 AB , I
+	FLG UXXXX AB sub %1 I
 }
 
-CMPA	i	|1 0 0 0 0 0 0 1| i		| {
-	%2 = sub i8 %A , I
-	FLG UXXXX %2 sub %A I
+CMP	AB,i	|1|a|0 0 0 0 0 1| i		| {
+	%2 = sub i8 AB , I
+	FLG UXXXX %2 sub AB I
 }
 
-SBCA	i	|1 0 0 0 0 0 1 0| i		|
+SBC	AB,i	|1|a|0 0 0 0 1 0| i		|
 
 SUBD	I	|1 0 0 0 0 0 1 1| I1		| I2		|
 
-ANDA	i	|1 0 0 0 0 1 0 0| i		|
+ADDD	I	|1 1 0 0 0 0 1 1| I1		| I2		|
 
-BITA	i	|1 0 0 0 0 1 0 1| i		|
+AND	AB,i	|1|a|0 0 0 1 0 0| i		|
 
-LDA	i	|1 0 0 0 0 1 1 0| i		|
+BIT	AB,i	|1|a|0 0 0 1 0 1| i		|
 
-EORA	i	|1 0 0 0 1 0 0 0| i		|
+LD	AB,i	|1|a|0 0 0 1 1 0| i		|
 
-ADCA	i	|1 0 0 0 1 0 0 1| i		|
+EOR	AB,i	|1|a|0 0 1 0 0 0| i		|
 
-ORA	i	|1 0 0 0 1 0 1 0| i		|
+ADC	AB,i	|1|a|0 0 1 0 0 1| i		|
 
-ADDA	i	|1 0 0 0 1 0 1 1| i		|
+OR	AB,i	|1|a|0 0 1 0 1 0| i		|
+
+ADD	AB,i	|1|a|0 0 1 0 1 1| i		|
+
 
 CMP	XY,I	|1 0 0 0 1 1 0 0| I1		| I2		|
 
@@ -338,29 +321,35 @@ LD	XY,I	|1 0 0 0 1 1 1 0| I1		| I2		| {
 	FLG -XX0- I16
 }
 
-SUBA	d	|1 0 0 1 0 0 0 0| d		|
+SUB	AB,d	|1|a|0 1 0 0 0 0| d		|
 
-CMPA	d	|1 0 0 1 0 0 0 1| d		|
+CMP	AB,d	|1|a|0 1 0 0 0 1| d		|
 
-SBCA	d	|1 0 0 1 0 0 1 0| d		|
+SBC	AB,d	|1|a|0 1 0 0 1 0| d		|
 
 SUBD	d	|1 0 0 1 0 0 1 1| d		|
 
-ANDA	d	|1 0 0 1 0 1 0 0| d		|
+ADDD	d	|1 1 0 1 0 0 1 1| d		|
 
-BITA	d	|1 0 0 1 0 1 0 1| d		|
+AND	AB,d	|1|a|0 1 0 1 0 0| d		|
 
-LDA	d	|1 0 0 1 0 1 1 0| d		|
+BIT	AB,d	|1|a|0 1 0 1 0 1| d		|
 
-STA	d	|1 0 0 1 0 1 1 1| d		|
+LD	AB,d	|1|a|0 1 0 1 1 0| d		|
 
-EORA	d	|1 0 0 1 1 0 0 0| d		|
+ST	AB,d	|1|a|0 1 0 1 1 1| d		|
 
-ADCA	d	|1 0 0 1 1 0 0 1| d		|
+EOR	AB,d	|1|a|0 1 1 0 0 0| d		|
 
-ORA	d	|1 0 0 1 1 0 1 0| d		|
+ADC	AB,d	|1|a|0 1 1 0 0 1| d		|
 
-ADDA	d	|1 0 0 1 1 0 1 1| d		|
+OR	AB,d	|1|a|0 1 1 0 1 0| d		|
+
+ADD	AB,d	|1|a|0 1 1 0 1 1| d		|
+
+LDD	d	|1 1 0 1 1 1 0 0| d		|
+
+STD	d	|1 1 0 1 1 1 0 1| d		|
 
 CMP	XY,d	|1 0 0 1 1 1 0 0| d		|
 
@@ -370,29 +359,31 @@ LD	XY,d	|1 0 0 1 1 1 1 0| d		|
 
 ST	XY,d	|1 0 0 1 1 1 1 1| d		|
 
-SUBA	P	|1 0 1 0 0 0 0 0|X| R |i| m	|
+SUB	AB,P	|1|a|1 0 0 0 0 0|X| R |i| m	|
 
-CMPA	P	|1 0 1 0 0 0 0 1|X| R |i| m	|
+CMP	AB,P	|1|a|1 0 0 0 0 1|X| R |i| m	|
 
-SBCA	P	|1 0 1 0 0 0 1 0|X| R |i| m	|
+SBC	AB,P	|1|a|1 0 0 0 1 0|X| R |i| m	|
 
 SUBD	P	|1 0 1 0 0 0 1 1|X| R |i| m	|
 
-ANDA	P	|1 0 1 0 0 1 0 0|X| R |i| m	|
+ADDD	P	|1 1 1 0 0 0 1 1|X| R |i| m     |
 
-BITA	P	|1 0 1 0 0 1 0 1|X| R |i| m	|
+AND	AB,P	|1|a|1 0 0 1 0 0|X| R |i| m	|
 
-LDA	P	|1 0 1 0 0 1 1 0|X| R |i| m	|
+BIT	AB,P	|1|a|1 0 0 1 0 1|X| R |i| m	|
 
-STA	P	|1 0 1 0 0 1 1 1|X| R |i| m	|
+LD	AB,P	|1|a|1 0 0 1 1 0|X| R |i| m	|
 
-EORA	P	|1 0 1 0 1 0 0 0|X| R |i| m	|
+ST	AB,P	|1|a|1 0 0 1 1 1|X| R |i| m	|
 
-ADCA	P	|1 0 1 0 1 0 0 1|X| R |i| m	|
+EOR	AB,P	|1|a|1 0 1 0 0 0|X| R |i| m	|
 
-ORA	P	|1 0 1 0 1 0 1 0|X| R |i| m	|
+ADC	AB,P	|1|a|1 0 1 0 0 1|X| R |i| m	|
 
-ADDA	P	|1 0 1 0 1 0 1 1|X| R |i| m	|
+OR	AB,P	|1|a|1 0 1 0 1 0|X| R |i| m	|
+
+ADD	AB,P	|1|a|1 0 1 0 1 1|X| R |i| m	|
 
 CMP	XY,P	|1 0 1 0 1 1 0 0|X| R |i| m	|
 
@@ -402,29 +393,31 @@ LD	XY,P	|1 0 1 0 1 1 1 0|X| R |i| m     |
 
 ST	XY,P	|1 0 1 0 1 1 1 1|X| R |i| m     |
 
-SUBA	E	|1 0 1 1 0 0 0 0| e1		| e2		|
+SUB	AB,E	|1|a|1 1 0 0 0 0| e1		| e2		|
 
-CMPA	E	|1 0 1 1 0 0 0 1| e1		| e2		|
+CMP	AB,E	|1|a|1 1 0 0 0 1| e1		| e2		|
 
-SBCA	E	|1 0 1 1 0 0 1 0| e1		| e2		|
+SBC	AB,E	|1|a|1 1 0 0 1 0| e1		| e2		|
 
 SUBD	E	|1 0 1 1 0 0 1 1| e1		| e2		|
 
-ANDA	E	|1 0 1 1 0 1 0 0| e1		| e2		|
+ADDD	E	|1 1 1 1 0 0 1 1| e1		| e2		|
 
-BITA	E	|1 0 1 1 0 1 0 1| e1		| e2		|
+AND	AB,E	|1 0 1 1 0 1 0 0| e1		| e2		|
 
-LDA	E	|1 0 1 1 0 1 1 0| e1		| e2		|
+BIT	AB,E	|1|a|1 1 0 1 0 1| e1		| e2		|
 
-STA	E	|1 0 1 1 0 1 1 1| e1		| e2		|
+LD	AB,E	|1|a|1 1 0 1 1 0| e1		| e2		|
 
-EORA	E	|1 0 1 1 1 0 0 0| e1		| e2		|
+ST	AB,E	|1|a|1 1 0 1 1 1| e1		| e2		|
 
-ADCA	E	|1 0 1 1 1 0 0 1| e1		| e2		|
+EOR	AB,E	|1|a|1 1 1 0 0 0| e1		| e2		|
 
-ORA	E	|1 0 1 1 1 0 1 0| e1		| e2		|
+ADC	AB,E	|1|a|1 1 1 0 0 1| e1		| e2		|
 
-ADDA	E	|1 0 1 1 1 0 1 1| e1		| e2		|
+OR	AB,E	|1|a|1 1 1 0 1 0| e1		| e2		|
+
+ADD	AB,E	|1|a|1 1 1 0 1 1| e1		| e2		|
 
 CMP	XY,E	|1 0 1 1 1 1 0 0| e1		| e2		|
 
@@ -437,28 +430,6 @@ LD	XY,E	|1 0 1 1 1 1 1 0| e1		| e2		| {
 
 ST	XY,E	|1 0 1 1 1 1 1 1| e1		| e2		|
 
-SUBB	i	|1 1 0 0 0 0 0 0| i		|
-
-CMPB	i	|1 1 0 0 0 0 0 1| i		|
-
-SBCB	i	|1 1 0 0 0 0 1 0| i		|
-
-ADDD	I	|1 1 0 0 0 0 1 1| I1		| I2		|
-
-ANDB	i	|1 1 0 0 0 1 0 0| i		|
-
-BITB	i	|1 1 0 0 0 1 0 1| i		|
-
-LDB	i	|1 1 0 0 0 1 1 0| i		|
-
-EORB	i	|1 1 0 0 1 0 0 0| i		|
-
-ADCB	i	|1 1 0 0 1 0 0 1| i		|
-
-ORB	i	|1 1 0 0 1 0 1 0| i		|
-
-ADDB	i	|1 1 0 0 1 0 1 1| i		|
-
 LDD	I	|1 1 0 0 1 1 0 0| I1		| I2		| {
 	%A = i8 I1
 	%B = i8 I2
@@ -470,61 +441,9 @@ LD	SU,I	|1 1 0 0 1 1 1 0| I1		| I2		| {
 	FLG -XX0- I16
 }
 
-SUBB	d	|1 1 0 1 0 0 0 0| d		|
-
-CMPB	d	|1 1 0 1 0 0 0 1| d		|
-
-SBCB	d	|1 1 0 1 0 0 1 0| d		|
-
-ADDD	d	|1 1 0 1 0 0 1 1| d		|
-
-ANDB	d	|1 1 0 1 0 1 0 0| d		|
-
-BITB	d	|1 1 0 1 0 1 0 1| d		|
-
-LDB	d	|1 1 0 1 0 1 1 0| d		|
-
-STB	d	|1 1 0 1 0 1 1 1| d		|
-
-EORB	d	|1 1 0 1 1 0 0 0| d		|
-
-ADCB	d	|1 1 0 1 1 0 0 1| d		|
-
-ORB	d	|1 1 0 1 1 0 1 0| d		|
-
-ADDB	d	|1 1 0 1 1 0 1 1| d		|
-
-LDD	d	|1 1 0 1 1 1 0 0| d		|
-
-STD	d	|1 1 0 1 1 1 0 1| d		|
-
 LD	SU,d	|1 1 0 1 1 1 1 0| d		|
 
 ST	SU,d	|1 1 0 1 1 1 1 1| d		|
-
-SUBB	P	|1 1 1 0 0 0 0 0|X| R |i| m     |
-
-CMPB	P	|1 1 1 0 0 0 0 1|X| R |i| m     |
-
-SBCB	P	|1 1 1 0 0 0 1 0|X| R |i| m     |
-
-ADDD	P	|1 1 1 0 0 0 1 1|X| R |i| m     |
-
-ANDB	P	|1 1 1 0 0 1 0 0|X| R |i| m     |
-
-BITB	P	|1 1 1 0 0 1 0 1|X| R |i| m     |
-
-LDB	P	|1 1 1 0 0 1 1 0|X| R |i| m     |
-
-STB	P	|1 1 1 0 0 1 1 1|X| R |i| m     |
-
-EORB	P	|1 1 1 0 1 0 0 0|X| R |i| m     |
-
-ADCB	P	|1 1 1 0 1 0 0 1|X| R |i| m     |
-
-ORB	P	|1 1 1 0 1 0 1 0|X| R |i| m     |
-
-ADDB	P	|1 1 1 0 1 0 1 1|X| R |i| m     |
 
 LDD	P	|1 1 1 0 1 1 0 0|X| R |i| m     |
 
@@ -533,30 +452,6 @@ STD	P	|1 1 1 0 1 1 0 1|X| R |i| m     |
 LD	SU,P	|1 1 1 0 1 1 1 0|X| R |i| m     |
 
 ST	SU,P	|1 1 1 0 1 1 1 1|X| R |i| m     |
-
-SUBB	E	|1 1 1 1 0 0 0 0| e1		| e2		|
-
-CMPB	E	|1 1 1 1 0 0 0 1| e1		| e2		|
-
-SBCB	E	|1 1 1 1 0 0 1 0| e1		| e2		|
-
-ADDD	E	|1 1 1 1 0 0 1 1| e1		| e2		|
-
-ANDB	E	|1 1 1 1 0 1 0 0| e1		| e2		|
-
-BITB	E	|1 1 1 1 0 1 0 1| e1		| e2		|
-
-LDB	E	|1 1 1 1 0 1 1 0| e1		| e2		|
-
-STB	E	|1 1 1 1 0 1 1 1| e1		| e2		|
-
-EORB	E	|1 1 1 1 1 0 0 0| e1		| e2		|
-
-ADCB	E	|1 1 1 1 1 0 0 1| e1		| e2		|
-
-ORB	E	|1 1 1 1 1 0 1 0| e1		| e2		|
-
-ADDB	E	|1 1 1 1 1 0 1 1| e1		| e2		|
 
 LDD	E	|1 1 1 1 1 1 0 0| e1		| e2		|
 
@@ -617,6 +512,9 @@ class mc6809_ins(assy.Instree_ins):
 
 	def assy_PFX11(self, pj):
 		self.pfx = 0x11
+
+	def assy_AB(self, pj):
+		self.mne += "AB"[self['a']]
 
 	def assy_XY(self, pj):
 		if self.pfx == 0x10:
@@ -876,6 +774,15 @@ class mc6809_ins(assy.Instree_ins):
 			[ "FLG", "-XX0-", r ],
 		])
 
+	def il_clr(self):
+		adr = self.il_adr()
+		if adr is None:
+			return
+		self.add_il([
+			[ "store", "i8", "0", ",", "i8*", adr ],
+			[ "FLG", "-1000" ],
+		])
+		
 	def ildefault(self):
 		mne1 = self.mne[:-1]
 		if mne1 in ('OR', 'AND', 'EOR'):
@@ -888,6 +795,11 @@ class mc6809_ins(assy.Instree_ins):
 			self.il_ld8()
 		if self.mne in ('STA', 'STB'):
 			self.il_st8()
+		if self.mne == "CLR":
+			self.il_clr()
+
+	def ilmacro_AB(self):
+		return "%" + "AB"[self['a']]
 
 	def ilmacro_BR(self):
 		cc = self['cc']
