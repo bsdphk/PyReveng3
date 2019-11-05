1634c8106aeac77899246ef9e1af732400650f14
Poul-Henning Kamp <phk@FreeBSD.org>
2019-11-05 20:05:11 +0000

    Add PIL support

--- pyreveng/cpu/mcs4.py
+++ pyreveng/cpu/mcs4.py
@@ -27,55 +27,174 @@
 '''Intel 4004/MCS-4
 
 "Worlds First Micro Processor"
+
+address_space(1) = data_ram_character
+address_space(2) = data_ram_status
+address_space(3) = rom_port
+address_space(4) = ram_port
+address_space(5) = TEST pin on i4004
 '''
 
 from pyreveng import assy
 
 mcs4_desc = """
 NOP	-		|0 0 0 0|0 0 0 0|
-JCN	cc,adr,>JC	|0 0 0 1| cc    | adr		|
-FIM	rr,data		|0 0 1 0| rr  |0| data		|
-SRC	rr		|0 0 1 0| rr  |1|
-FIN	rr,(rr0)	|0 0 1 1| rr  |0|
+JCN	cc,adr,>JC	|0 0 0 1| cc    | adr		| {
+	JCN
+}
+FIM	rr,data		|0 0 1 0| rr  |0| data		| {
+	RR = i8 DATA
+}
+SRC	rr		|0 0 1 0| rr  |1| {
+	%SRC = i8 RR
+}
+FIN	rr,(rr0)	|0 0 1 1| rr  |0| {
+	%0 = i12 PCT
+	%1 = zext i8 %RR0 to i12
+	%2 = add i12 %0 , %1
+	RR = load i8 , i8* %2
+}
 JIN	rr		|0 0 1 1| rr  |1|
-JUN	ladr,>J		|0 1 0 0| ahi	| alo		|
-JMS	ladr,>C		|0 1 0 1| ahi	| alo		|
-INC	r		|0 1 1 0| r	|
-ISZ	r,adr,isz,>JC	|0 1 1 1| r	| adr		|
-ADD	r		|1 0 0 0| r	|
-SUB	r		|1 0 0 1| r	|
-LD	r		|1 0 1 0| r	|
-XCH	r		|1 0 1 1| r	|
-BBL	d,>R		|1 1 0 0| d	|
-LDM	d		|1 1 0 1| d	|
-WRM	-		|1 1 1 0|0 0 0 0|
-WMP	-		|1 1 1 0|0 0 0 1|
-WRR	-		|1 1 1 0|0 0 1 0|
+JUN	ladr,>J		|0 1 0 0| ahi	| alo		| {
+	br label ADR
+}
+JMS	ladr,>C		|0 1 0 1| ahi	| alo		| {
+	call label ADR
+}
+INC	r		|0 1 1 0| r	| {
+	R = add i4 R , 1
+}
+ISZ	r,adr,isz,>JC	|0 1 1 1| r	| adr		| {
+	R = add i4 R , 1
+	%0 = icmp i4 R , 0
+	br i1 %0 , SKIP , NEXT
+}
+ADD	r		|1 0 0 0| r	| {
+	%0 = zext i1 %C to i4
+	%C = pyreveng.carry.add ( %A , R , %0 )
+	%1 = add i4 %A , %0
+	%A = add i4 %1, R
+}
+# XXX: IL: %C inverted ?
+SUB	r		|1 0 0 1| r	| {
+	%0 = zext i1 %C to i4
+	%1 = xor i4 %0 , 0x1
+	%2 = xor i4 R , 0xf
+	%C = pyreveng.carry.add ( %A , %2 , %1 )
+	%3 = add i4 %A , %1
+	%A = add i4 %3, R
+}
+LD	r		|1 0 1 0| r	| {
+	%A = i4 R
+}
+XCH	r		|1 0 1 1| r	| {
+	%0 = i4 %A
+	%A = i4 R
+	R = i4 %0
+}
+BBL	d,>R		|1 1 0 0| d	| {
+	%A = i4 D
+	return
+}
+LDM	d		|1 1 0 1| d	| {
+	%A = i4 D
+}
+WRM	-		|1 1 1 0|0 0 0 0| {
+	store i4 %A, i4 addrspace(1) * %SPC
+}
+WMP	-		|1 1 1 0|0 0 0 1| {
+	store i4 %A, i4 addrspace(4) * %SPC
+}
+WRR	-		|1 1 1 0|0 0 1 0| {
+	store i4 %A, i4 addrspace(3) * %SPC
+}
 WPM	-		|1 1 1 0|0 0 1 1|
-WR0	-		|1 1 1 0|0 1 0 0|
-WR1	-		|1 1 1 0|0 1 0 1|
-WR2	-		|1 1 1 0|0 1 1 0|
-WR3	-		|1 1 1 0|0 1 1 1|
-SBM	-		|1 1 1 0|1 0 0 0|
-RDM	-		|1 1 1 0|1 0 0 1|
-RDR	-		|1 1 1 0|1 0 1 0|
-ADM	-		|1 1 1 0|1 0 1 1|
-RD0	-		|1 1 1 0|1 1 0 0|
-RD1	-		|1 1 1 0|1 1 0 1|
-RD2	-		|1 1 1 0|1 1 1 0|
-RD3	-		|1 1 1 0|1 1 1 1|
-CLB	-		|1 1 1 1|0 0 0 0|
-CLC	-		|1 1 1 1|0 0 0 1|
-IAC	-		|1 1 1 1|0 0 1 0|
-CMC	-		|1 1 1 1|0 0 1 1|
-CMA	-		|1 1 1 1|0 1 0 0|
-RAL	-		|1 1 1 1|0 1 0 1|
-RAR	-		|1 1 1 1|0 1 1 0|
-TCC	-		|1 1 1 1|0 1 1 1|
-DAC	-		|1 1 1 1|1 0 0 0|
+WR	-		|1 1 1 0|0 1|sr | {
+	%0 = zext i1 %SPC to i10
+	%1 = %0 or i10 %0 , SR
+	store i4 %A, i4 addrspace(2) * %1
+}
+SBM	-		|1 1 1 0|1 0 0 0| {
+	%0 = load i4 addrspace(1)* %SPC
+	%1 = zext i1 %C to i4
+	%2 = xor i4 %1 , 0x1
+	%3 = xor i4 %0 , 0xf
+	%C = pyreveng.carry.add ( %A , %3 , %2 )
+	%4 = add i4 %A , %1
+	%A = add i4 %4, %0
+}
+RDM	-		|1 1 1 0|1 0 0 1| {
+	%A = load i4 addrspace(1)* %SPC
+}
+RDR	-		|1 1 1 0|1 0 1 0| {
+	%A = load i4 addrspace(3)* %SPC
+}
+ADM	-		|1 1 1 0|1 0 1 1| {
+	%0 = load i4 addrspace(1)* %SPC
+	%1 = zext i1 %C to i4
+	%C = pyreveng.carry.add ( %A , %0 , %1 )
+	%2 = add i4 %A , %0
+	%A = add i4 %1, %2
+}
+RD	sr		|1 1 1 0|1 1|sr | {
+	%0 = zext i1 %SPC to i10
+	%1 = %0 or i10 %0 , SR
+	%A = load i4 addrspace(2)* %1
+}
+CLB	-		|1 1 1 1|0 0 0 0| {
+	%A = i4 0
+	%C = i1 0
+}
+CLC	-		|1 1 1 1|0 0 0 1| {
+	%C = i1 0
+}
+IAC	-		|1 1 1 1|0 0 1 0| {
+	%0 = zext i1 %C to i4
+	%C = pyreveng.carry.add ( %A , %0 , 1 )
+	%1 = add i4 %A , 1
+	%A = add i4 %1 , %0
+}
+CMC	-		|1 1 1 1|0 0 1 1| {
+	%C = xor i1 %C , 1
+}
+CMA	-		|1 1 1 1|0 1 0 0| {
+	%A = xor i4 %A , 0xf
+}
+RAL	-		|1 1 1 1|0 1 0 1| {
+	%0 = shr i4 %A , 3
+	%1 = shl i4 %A , 1
+	%2 = zext i1 %C to i4
+	%C = trunc i4 %0 , i1
+	%A = or i4 %1 , %2
+}
+RAR	-		|1 1 1 1|0 1 1 0| {
+	%1 = zext i1 %C to i4
+	%C = trunc i4 %A to i1
+	%2 = shr i4 %A , 1
+	%3 = shl i4 %1 , 3
+	%A = or i4 %2 , %3
+}
+TCC	-		|1 1 1 1|0 1 1 1| {
+	%A = zext i1 %C to i4
+	%C = i1 0
+}
+DAC	-		|1 1 1 1|1 0 0 0| {
+	%C = icmp ne i4 %A 0xf 
+	%A = sub i4 %A , 1
+}
 TCS	-		|1 1 1 1|1 0 0 1|
-STC	-		|1 1 1 1|1 0 1 0|
-DAA	-		|1 1 1 1|1 0 1 1|
+STC	-		|1 1 1 1|1 0 1 0| {
+	%C = i1 1
+}
+DAA	-		|1 1 1 1|1 0 1 1| {
+	%0 = icmp gt i4 %A , 9
+	%1 = or i1 %C , %0
+	br %1 label %2 , NEXT
+%2:
+	%3 = pyreveng.carry.add ( %A , 6 )
+	%A = add i4 %A , 6
+	%C = or i1 %C , %3
+}
 KBP	-		|1 1 1 1|1 1 0 0|
 DCL	-		|1 1 1 1|1 1 0 1|
 """
@@ -101,6 +220,9 @@ class mcs4_ins(assy.Instree_ins):
             self.cc = "CC#0x%x" % self['cc']
         return self.cc
 
+    def assy_sr(self):
+        self.mne += "%d" % self['sr']
+
     def assy_r(self):
         return "r%d" % self['r']
 
@@ -117,6 +239,51 @@ class mcs4_ins(assy.Instree_ins):
 
     def assy_isz(self):
         self.cc = "Z"
+
+    def pilmacro_R(self):
+        return "%%R%d" % self['r']
+
+    def pilmacro_RR(self):
+        return "%%RR%d" % (self['rr'] << 1)
+
+    def pilmacro_PCT(self):
+        return "0x%03x" % (self.lo & 0xf00)
+
+    def pilmacro_DATA(self):
+        return "0x%02x" % self['data']
+
+    def pilmacro_D(self):
+        return "0x%01x" % self['d']
+
+    def pilmacro_ADR(self):
+        return "0x%01x%02x" % (self['ahi'], self['alo'])
+
+    def pilmacro_SR(self):
+        return "0x%03x" % (self['sr'] << 8)
+
+    def pilmacro_NEXT(self):
+        return "0x%03x" % self.hi
+
+    def pilmacro_SKIP(self):
+        return "0x%01x%02x" % (self.hi >> 8, self['adr'])
+
+    def pilmacro_JCN(self):
+        cc = self['cc']
+        l = []
+        l.append(["%0", "=", "i1", "0"])
+        if cc & 0x4:
+            l.append(["%1", "=", "icmp", "eq", "i4", "%A", ",", "0"])
+            l.append(["%0", "=", "or", "i1", "%0", ",", "%1"])
+        if cc & 0x2:
+            l.append(["%0", "=", "or", "i1", "%0", ",", "%C"])
+        if cc & 0x1:
+            l.append(["%2", "=", "load", "i1", "addrspace", "(", "5", ")", "*", "0"])
+            l.append(["%0", "=", "or", "i1", "%0", ",", "%TEST"])
+        if cc & 0x8:
+            l.append(["br", "i1", "%0", ",", "NEXT", ",", "SKIP"])
+        else:
+            l.append(["br", "i1", "%0", ",", "SKIP", ",", "NEXT"])
+        self.add_il(l)
 
 class mcs4(assy.Instree_disass):
     def __init__(self):
